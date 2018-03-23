542da6baa08e897156572f8ae78abe73127d572e
dhrubesh <dhrubesh97@gmail.com>
2018-03-23 17:42:06 +0530

    courses type should be array (fixed)

--- fast-coding-GraphQL/new_server.js
+++ fast-coding-GraphQL/new_server.js
@@ -6,7 +6,7 @@ const {buildSchema} = require('graphql');
 var schema = buildSchema(`
 	type Query{
 		course(id: Int!): Course
-		courses(topic: String): Course
+		courses(topic: String): [Course]
 	}
 	type Course{
 		id: Int
