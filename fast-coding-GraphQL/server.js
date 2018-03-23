58fb4667c2cf156dea692d87e606b62130c384c0
dhrubesh <dhrubesh97@gmail.com>
2018-03-23 17:29:34 +0530

    added GraphQL end point into the server

--- fast-coding-GraphQL/server.js
+++ fast-coding-GraphQL/server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const express_graphql = require('express-graphql');
 const {buildSchema} = require('graphql');
-app = express();
-app.get('/', (req, res) => res.send('Hello World!'));
 
 //GraphQL Schema
 var schema = buildSchema(`
@@ -16,5 +14,11 @@ var root = {
 	message: () => 'Hello World'
 }
 
-
+//Express Server and GraphQL endpoint
+app = express();
+app.use('/graphql',express_graphql({
+	schema: schema,
+	rootValue: root,
+	graphiql: true
+}))
 app.listen(4000,() => console.log('listening at port 4000'));
