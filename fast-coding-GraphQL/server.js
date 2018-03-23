0a58814e815664dbd7ce5a5f4726453877f89111
dhrubesh <dhrubesh97@gmail.com>
2018-03-23 17:07:44 +0530

    added GraphQL schema

--- fast-coding-GraphQL/server.js
+++ fast-coding-GraphQL/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const express_graphql = require('express-graphql');
 const {buildSchema} = require('graphql');
-
 app = express();
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
+
+//GraphQL Schema
+var schema = buildSchema(`
+	type Query{
+		message: String
+	}
+`)
+
 
 app.listen(4000,() => console.log('listening at port 4000'));
